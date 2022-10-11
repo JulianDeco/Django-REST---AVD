27ee2ec79eaf28337b4203a341fb92868b06e390
JulianDeco <102333273+JulianDeco@users.noreply.github.com>
2022-10-11 20:14:01 -0300

    A

--- apiExpo/settings.py
+++ apiExpo/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-'django-insecure-49ci*1(vj(u_1$p-%y%4ak^+o1(4mkw8^kno!^d%&0nx(h=t$+'
+
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
