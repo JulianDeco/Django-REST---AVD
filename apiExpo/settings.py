7ce3f28fb3d7f718a09cdec0bee5bc4c12c5f084
JulianDeco <102333273+JulianDeco@users.noreply.github.com>
2022-10-11 23:27:02 -0300

    Update settings.py

--- apiExpo/settings.py
+++ apiExpo/settings.py
@@ -91,8 +91,7 @@ WSGI_APPLICATION = 'apiExpo.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
