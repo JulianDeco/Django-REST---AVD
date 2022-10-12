a35170eca1dfaddc7f4bc29ca79a44c8d2373ca8
JulianDeco <102333273+JulianDeco@users.noreply.github.com>
2022-10-11 22:35:48 -0300

    Update settings.py

--- apiExpo/settings.py
+++ apiExpo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,7 +92,7 @@ WSGI_APPLICATION = 'apiExpo.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
