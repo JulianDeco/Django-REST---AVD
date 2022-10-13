ac684d208bbe0729180e4af74cef458d212e1781
JulianDeco <102333273+JulianDeco@users.noreply.github.com>
2022-10-13 12:01:50 -0300

    Hola

--- apiExpo/settings.py
+++ apiExpo/settings.py
@@ -10,8 +10,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+<<<<<<< .merge_file_a08064
 
 import dj_database_url
+=======
+>>>>>>> .merge_file_a10892
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -92,7 +96,12 @@ WSGI_APPLICATION = 'apiExpo.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
+<<<<<<< .merge_file_a08064
         default='sqlite:///db.sqlite3',
+=======
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+>>>>>>> .merge_file_a10892
         conn_max_age=600
     )
 }
@@ -141,12 +150,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+<<<<<<< .merge_file_a08064
 
 if not DEBUG:    
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+=======
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+>>>>>>> .merge_file_a10892
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
