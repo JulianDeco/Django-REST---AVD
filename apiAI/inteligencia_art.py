fa6c60193d8dd741ae1e8a51d2900dc7f9bee918
JulianDeco <102333273+JulianDeco@users.noreply.github.com>
2022-10-17 14:25:18 -0300

    cambios en temperature OpenAI

--- apiAI/inteligencia_art.py
+++ apiAI/inteligencia_art.py
@@ -2,7 +2,7 @@ import openai
 import os
 from googletrans import Translator
 import json
-
+from datetime import datetime
 """
 INTENTAR CON VOZ
 IF VOZ O TECLADO
@@ -10,14 +10,16 @@ IF VOZ O TECLADO
 SE PUEDE ENVIAR EN JSON TAMBIÉN
 """
 
-# translator = Translator()
+translator = Translator()
 
 def gpt3(stext):
+    date = datetime.now()
+    print(date)
     openai.api_key= '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'
     response = openai.Completion.create(
     engine="text-davinci-002",
     prompt=stext,
-    temperature=0.6,
+    temperature=0.8,
     max_tokens=220,
     top_p=1.0,
     frequency_penalty=0.0,
@@ -29,8 +31,16 @@ def gpt3(stext):
     # arreglo = content[0]
     response = StrContent.strip('\n\n')
     # content[0] = response
-
+    A = translator.detect(response)
+    if A.lang == 'es':
+        print(A)
     
+    
+    
+    date = datetime.now()
+    print(date)
+
     return response
 
-print(gpt3('Como es el final del mundo?'))
+
+print(gpt3('¿Que es un cometa?'))
